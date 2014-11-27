a89db0e8f1b486cab8952d91c40ebd8270b8ee74
Aneeket Patil <aneeketp@qti.qualcomm.com>
2014-11-26 16:41:48 -0800

    Add xml file generation

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import xml.etree.cElementTree as ET
 
 class MyClass:
     """A simple example class"""
@@ -12,3 +13,17 @@ if __name__ == "__main__":
     x = MyClass()
     print x.f()
     print x.i
+    root = ET.Element("root")
+
+    doc = ET.SubElement(root, "doc")
+
+    field1 = ET.SubElement(doc, "field1")
+    field1.set("name", "blah")
+    field1.text = "some value1"
+
+    field2 = ET.SubElement(doc, "field2")
+    field2.set("name", "asdfasd")
+    field2.text = "some vlaue2"
+
+    tree = ET.ElementTree(root)
+    tree.write("filename.xml")
